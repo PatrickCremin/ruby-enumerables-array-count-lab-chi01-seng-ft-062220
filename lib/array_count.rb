1288e48d3be53328592acb526a555d92d3278fb5
Patrick Cremin <patrickfxcremin@gmail.com>
2020-06-08 14:19:17 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,6 +1,7 @@
 def count_strings(array_1, array_2)
   count_strings(array_1)
   array_1 = ["", "hello", "world", 4, 1, 5, [], {}]
+  count_strings(array_2)
 array_2 = ["", 4, "", "goodbye",""]
 end
 
